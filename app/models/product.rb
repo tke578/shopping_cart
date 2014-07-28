6c9801d6fa395c1a3a2ba29c197d773bcc932a98
Adnrew G <tke578@gmail.com>
2014-07-28 00:14:22 -0700

    Added test fixtures

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :description, :image_url, presence: true
+	validates :title, :presence => {:message => "Title can't be blank." }, :uniqueness => {:message => "has already been taken"}, presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validate :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
